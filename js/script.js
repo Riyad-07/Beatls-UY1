f904450a5d507107b206c0d2d5b259cda1a0cd14
Riyad-07 <samimsamim1998@gmail.com>
2024-05-21 22:02:48 +0600

    PSD to HTML

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$('.slider').slick({
+    infinite: true,
+    arrows: false,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    dots: false,
+    speed: 1000,
+    fade: true,
+});
+
+$('.slider101').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow: '<i class="fa-solid fa-angle-left prev"></i>',
+  nextArrow: '<i class="fa-solid fa-angle-right next"></i>'
+});
+
